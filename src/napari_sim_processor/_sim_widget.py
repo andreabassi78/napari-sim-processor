f34baad814f1f6e87c2ce12753b6df0e1426ff9f
andreabassi78 <andreabassi@polimi.it>
2023-01-30 13:45:13 +0100

    magic_gui_problems fixed

--- src/napari_sim_processor/_sim_widget.py
+++ src/napari_sim_processor/_sim_widget.py
@@ -3,11 +3,11 @@ Created on Tue Jan 25 16:34:41 2022
 
 @authors: Andrea Bassi @Polimi, Mark Neil @ImperialCollege
 """
-from napari_sim_processor.widget_settings import Setting, Combo_box
-from napari_sim_processor.baseSimProcessor import pytorch, cupy
-from napari_sim_processor.hexSimProcessor import HexSimProcessor
-from napari_sim_processor.raSimProcessor import RaSimProcessor
-from napari_sim_processor.convSimProcessor import ConvSimProcessor
+from widget_settings import Setting, Combo_box
+from baseSimProcessor import pytorch, cupy
+from hexSimProcessor import HexSimProcessor
+from raSimProcessor import RaSimProcessor
+from convSimProcessor import ConvSimProcessor
 import napari
 from qtpy.QtWidgets import QVBoxLayout, QSplitter, QHBoxLayout, QWidget, QPushButton, QLineEdit
 from qtpy.QtCore import Qt
@@ -124,6 +124,13 @@ def reshape(viewer: napari.Viewer,
         viewer.dims.set_point(axis=[0,1,2], value=[0,0,0]) #raises ValueError in napari versions <0.4.13 
 
 
+
+@magic_factory    
+def choose_layer(image: Image):
+        pass
+
+
+
 class SimAnalysis(QWidget):
     '''
     A Napari plugin for the reconstruction of Structured Illumination microscopy (SIM) data with GPU acceleration (with pytorch, if installed).
@@ -163,7 +170,9 @@ class SimAnalysis(QWidget):
         right_layout = QVBoxLayout()
         bottom_layout.addLayout(right_layout)
         # Fill top layout
-        self.add_magic_function(self.select_layer, top_layout)
+        self.choose_layer_widget = choose_layer()
+        self.choose_layer_widget.call_button.visible = False
+        self.add_magic_function(self.choose_layer_widget, top_layout)
         # Fill bottom-left layout
         self.sim_mode = Combo_box(name = 'Mode', choices = Sim_modes, layout=left_layout,
                                   write_function = self.reset_processor)
@@ -241,7 +250,8 @@ class SimAnalysis(QWidget):
                                   layout=right_layout,
                                   write_function = self.setReconstructor)    
         # buttons
-        buttons_dict = {'Widefield': self.calculate_WF_image,
+        buttons_dict = {'Select image layer': self.select_layer,
+                        'Widefield': self.calculate_WF_image,
                         'Calibrate': self.calibration,
                         'Plot calibration phases':self.find_phaseshifts,
                         'SIM reconstruction': self.single_plane_reconstruction,
@@ -257,24 +267,28 @@ class SimAnalysis(QWidget):
         self.messageBox.setText('Messages')
     
         
-    def add_magic_function(self, function, _layout):
-        self.viewer.layers.events.inserted.connect(function.reset_choices)
-        self.viewer.layers.events.removed.connect(function.reset_choices)
-        _layout.addWidget(function.native)
+    def add_magic_function(self, widget, _layout):
+        self.viewer.layers.events.inserted.connect(widget.reset_choices)
+        self.viewer.layers.events.removed.connect(widget.reset_choices)
+        _layout.addWidget(widget.native)
         
     
-    @magicgui(call_button='Select image layer')    
-    def select_layer(self, image: Image):
+    def select_layer(self):
         '''
-        Selects a Image layer after chaking that it contains raw sim data organized
+        Selects a Image layer after checking that it contains raw sim data organized
         as a 5D stack (angle,phase,z,y,x).
         Stores the name of the image in self.imageRaw_name, which is used frequently in the other methods.
         Parameters
         ----------
         image : napari.layers.Image
             The image layer to process, it contains the raw data 
-        '''      
+        '''
+        print(self.choose_layer_widget)
+        print(self.choose_layer_widget.image.value)
+        image = self.choose_layer_widget.image.value
+
         if not isinstance(image, Image):
+            print('Please select a correct image stack')
             return
         if hasattr(self,'imageRaw_name'):
             delattr(self,'imageRaw_name')
@@ -340,7 +354,7 @@ class SimAnalysis(QWidget):
             t0 = time.time()
             if self.step_changed(0,2,3,4):
                 self.setReconstructor()
-            delta_t = time.time() - t0
+            delta_t = int(time.time() - t0)
             self.on_step_change.set_timeout(delta_t)
 
 
@@ -380,8 +394,8 @@ class SimAnalysis(QWidget):
             layer = self.viewer.add_image(image_values,
                                             name = im_name,
                                             scale = scale,
-                                            colormap = colormap,
-                                            interpolation = 'bilinear')
+                                            colormap = colormap)
+            layer.interpolation2d = 'spline36'
         self.center_stack(image_values)
         # self.move_layer_to_top(layer)
         if kwargs.get('autoscale') is True:
