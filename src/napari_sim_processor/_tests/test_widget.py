61c00f03b0ab04d035481977fbc8b772f95aa047
andreabassi78 <andreabassi@polimi.it>
2023-02-01 14:49:04 +0100

    testing sim_widget plugin with tox

--- src/napari_sim_processor/_tests/test_widget.py
+++ src/napari_sim_processor/_tests/test_widget.py
@@ -4,10 +4,10 @@ import numpy as np
 def test_sim_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer() 
     sim_widget = SimAnalysis(viewer)
-    viewer.add_image(np.random.random((3,3,9,100, 100)))
+    viewer.add_image(np.random.random((3,3,9,100, 90)))
     sim_widget.select_layer(viewer.layers[0])
     sim_widget.calculate_WF_image()
-    assert viewer.layers[1].data.shape == (9,100,100)
+    assert viewer.layers[1].data.shape == (9,100,90)
 
 def test_reshape_widget(make_napari_viewer, capsys):
     viewer = make_napari_viewer()
