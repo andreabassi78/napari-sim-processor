2c2a30234e89bb9e341023ad7d8ba860f1879b80
Mark Neil <mark.neil@imperial.ac.uk>
2023-02-12 13:11:37 +0000

    Modified runner to tabify widgets.  Fixed a couple of errors where self.N had not been changed to self.Nx or self.Ny

--- sim_widget_runner.py
+++ sim_widget_runner.py
@@ -3,16 +3,15 @@ from src.napari_sim_processor._sim_widget import reshape, SimAnalysis
 Script that runs the napari widget from the IDE. 
 It is not executed when the plugin runs.
 '''
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     import napari
     viewer = napari.Viewer()
     widget = SimAnalysis(viewer)
     my_reshape_widget = reshape()    
-    viewer.window.add_dock_widget(my_reshape_widget, name = 'Reshape stack', add_vertical_stretch = True)
-    viewer.window.add_dock_widget(widget,
+    w1 = viewer.window.add_dock_widget(my_reshape_widget, name = 'Reshape stack', add_vertical_stretch = True)
+    w2 = viewer.window.add_dock_widget(widget,
                                   name = 'Sim analyzer @Polimi',
                                   add_vertical_stretch = True)
-    
-    napari.run() 
-     
+    viewer.window._qt_window.tabifyDockWidget(w1, w2)
+    napari.run()
